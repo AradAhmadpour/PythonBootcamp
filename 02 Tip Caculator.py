ee2ffca28c9ced0161754e902820e40f4004a44b
AradAhmadpour <apply.arad@gmail.com>
2024-03-13 10:02:03 +0330

    Update 02 Tip Caculator.py

--- 02 Tip Caculator.py
+++ 02 Tip Caculator.py
@@ -1,6 +1,13 @@
 # Getting Data From User
 print("Welcome to Tip Caculator")
-Total_Bill= input("What wat the total bill? ")
-Tip_Percentage= input("what percentage tip would you like to give?")
-People= input("How many people to split the bill? ")
+Total_Bill= float(input("What wat the total bill? "))
+Tip_Percentage= int(input("what percentage tip would you like to give?"))
+People= int(input("How many people to split the bill? "))
 
+# Convert Tip Percentage to num
+Tip_Percentage_num = (1 + Tip_Percentage/100)
+
+# Caculation
+Tip_price= ((Total_Bill / People) * Tip_Percentage_num)
+Final_amount= (round(Tip_price, 2))
+print(f"Each Person should pay {Final_amount}")
